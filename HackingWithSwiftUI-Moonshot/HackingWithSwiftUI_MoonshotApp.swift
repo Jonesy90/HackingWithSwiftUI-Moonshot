0d38e36595d56d7f0d0bdd2605d44f1def6805a0
Michael Jones <michael.jones90@me.com>
2026-06-16 06:47:22 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-Moonshot/HackingWithSwiftUI_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_MoonshotApp.swift
+//  HackingWithSwiftUI-Moonshot
+//
+//  Created by Michael Jones on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
