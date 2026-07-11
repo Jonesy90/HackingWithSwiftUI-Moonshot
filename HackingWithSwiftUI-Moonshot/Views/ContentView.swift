05b844c0a2e5b104cc950a91c667fc0663f8a5b5
Michael Jones <michael.jones90@me.com>
2026-07-11 14:51:17 +0100

    xcode project update

--- HackingWithSwiftUI-Moonshot/Views/ContentView.swift
+++ HackingWithSwiftUI-Moonshot/Views/ContentView.swift
@@ -5,18 +5,6 @@
 //  Created by Michael Jones on 16/06/2026.
 //
 
-/*
- Challenge:
- 
- 1. Add the launch date to MissionView, below the mission badge. You might choose to format this differently given that more space is available, but it’s down to you.
- 2. Extract one or two pieces of view code into their own new SwiftUI views – the horizontal scroll view in MissionView is a great candidate, but if you followed my styling then you could also move the Rectangle dividers out too.
- 3. For a tough challenge, add a toolbar item to ContentView that toggles between showing missions as a grid and as a list.
- */
-
-/* Navigation Challenge
- 1. Return to project 8 (Moonshot), and upgrade it to use NavigationLink(value:). This means adding Hashable conformance, and thinking carefully how to use navigationDestination().
-*/
-
 import SwiftUI
 
 struct ContentView: View {
